0210997ef17bfcf7a521231843762a637c424abb
Malcolm Sparrow <msparrow@maksw.com>
2020-01-08 09:36:00 +0000

    Update

--- vendor/bat-native-ledger/src/bat/ledger/internal/promotion/promotion_unittest.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/promotion/promotion_unittest.cc
@@ -31,10 +31,7 @@ class PromotionTest : public testing::Test {
       mock_ledger_client_ = std::make_unique<ledger::MockLedgerClient>();
       mock_ledger_impl_ = std::make_unique<bat_ledger::MockLedgerImpl>
           (mock_ledger_client_.get());
-  }
-
-  void SetUp() override {
-    promotion_ = std::make_unique<Promotion>(mock_ledger_impl_.get());
+      promotion_ = std::make_unique<Promotion>(mock_ledger_impl_.get());
   }
 };
 
@@ -108,99 +105,144 @@ TEST_F(PromotionTest, TestFetchWithNoWalletPassphrase) {
   // Assert
   EXPECT_TRUE(callback_called);
 }
-// Mock Ledger w/ payment id
-// Mock Ledger w/ client info
-// Mock Ledger w/ braveledger_request_util::GetFetchPromotionUrl
-// SUCCESS: 
-// - expect ledger_->LoadURL with expected URL
-// OR expect callback Promotion::OnFetch with expected URL
+
 TEST_F(PromotionTest, TestFetch) {
-/*  
   // Arrange
-  EXPECT_CALL(*mock_ledger_impl_,
-      LoadURL(, _));
+  EXPECT_CALL(*mock_ledger_impl_, LoadURL(_, _, _, _, _, _));
+
   const std::string payment_id = "bob";
   ON_CALL(*mock_ledger_impl_, GetPaymentId())
       .WillByDefault(testing::ReturnRef(payment_id));
+
   const std::string wallet_passphrase = "fred";
   ON_CALL(*mock_ledger_impl_, GetWalletPassphrase())
       .WillByDefault(testing::Return(wallet_passphrase));
 
-  bool callback_called = false;
-
   ledger::FetchPromotionCallback fetch_promotion_callback =
       std::bind(
-          [&callback_called](ledger::Result result,
+          [&](ledger::Result result,
               ledger::PromotionList promotions) {
-            callback_called = true;
-            EXPECT_EQ(result, ledger::Result::CORRUPTED_WALLET);
           },
       _1, _2);
 
   // Act
   promotion_->Fetch(fetch_promotion_callback);
-
-  // Assert
-  EXPECT_TRUE(callback_called);
-*/  
 } 
 
 // void Claim(
 //     const std::string& payload,
 //     ledger::ClaimPromotionCallback callback);
 // SUCCESS: Expect call Promotion::OnCompletedAttestation
-TEST_F(PromotionTest, TestClaim) {
-  // Arrange
-
-  // Act
-
-  // Assert
-}
+// Nb. Needs a mock on AttestationImpl but cannot inject for testing
 
 // void Attest(
 //     const std::string& promotion_id,
 //     const std::string& solution,
 //     ledger::AttestPromotionCallback callback);
+// Nb. Needs a mock on AttestationImpl but cannot inject for testing
 
 // void Refresh(const bool retry_after_error);
-// FAIL: 
+TEST_F(PromotionTest, TestRefreshWithoutRetryWithTimerId) {
+  // Arrange
+  EXPECT_CALL(*mock_ledger_impl_, SetTimer(_, _)).Times(0);
+
+  // Act
+  promotion_->Refresh(false);
+}
+
+TEST_F(PromotionTest, TestRefreshWithRetryWithTimerId) {
+  // Arrange
+  EXPECT_CALL(*mock_ledger_impl_, SetTimer(_, _)).Times(0);
+
+  // Act
+  promotion_->Refresh(true);
+}
+/*
+TEST_F(PromotionTest, TestRefreshWithoutRetryOrTimerId) {
+  // Arrange
+  EXPECT_CALL(*mock_ledger_impl_, SetTimer(_, _)).Times(1);
+
+  // TODO: last_check_timer_id == 0
+
+  // Act
+  promotion_->Refresh(false);
+}
+
+TEST_F(PromotionTest, TestRefreshWithoutRetryOrTimerId) {
+  // Arrange
+  EXPECT_CALL(*mock_ledger_impl_, SetTimer(_, _)).Times(1);
+// calls ledger_->SetTimer with start_timer_in as a random value (how to test?)
+
+  // TODO: last_check_timer_id == 0
+
+  // Act
+  promotion_->Refresh(true);
+}
+*/
 
 // void OnTimer(const uint32_t timer_id);
+// Nb. Cannot test without mocking Promotion - discussed with tmancey and no value as code may be changing
+//   need to confirm with nejc
 // SUCCESS: timer_id == last_check_timer_id_;
 // - expect call Fetch
 // - last_check_timer_id_ = 0
-TEST_F(PromotionTest, TestOnTimerWithLastCheckTimerID) {
-  // Arrange
-
-  // Act
-
-  // Assert
-}
-
 // SUCCESS: timer_id == retry_timer_id_;
 // - expect call ledger_->GetAllPromotions
 // OR expect call Promotion::Retry
-TEST_F(PromotionTest, TestOnTimerWithRetryTimerID) {
-  // Arrange
-
-  // Act
-
-  // Assert
-}
-
 // FAIL: timer_id != (last_check_timer_id_ or retry_timer_id_)
 // - neither expected (above) to be called
-TEST_F(PromotionTest, TestOnTimerWithUknownTimerID) {
-  // Arrange
 
-  // Act
-
-  // Assert
-}
 
 // void ClaimTokens(
 //     ledger::PromotionPtr promotion,
 //     ledger::ResultCallback callback);
+TEST_F(PromotionTest, TestClaimTokensWithNullPromotion) {
+  // Arrange
+  bool callback_called = false;
+  ledger::ResultCallback callback =
+      std::bind(
+        [&callback_called](ledger::Result result) {
+          callback_called = (result == ledger::Result::LEDGER_ERROR);
+        }, _1);
+
+  // Act
+  promotion_->ClaimTokens(nullptr, callback);
+
+  // Assert
+  EXPECT_TRUE(callback_called);
+}
+
+TEST_F(PromotionTest, TestClaimTokensWithPromotion) {
+  // Arrange
+  EXPECT_CALL(*mock_ledger_impl_, LoadURL(_, _, _, _, _, _));
+ 
+  bool callback_called = false;
+
+  ledger::PromotionPtr promotion = ledger::Promotion::New();
+  promotion->id = "ABC123";
+  promotion->suggestions = 1;
+
+  ledger::ResultCallback callback =
+      std::bind(
+        [&callback_called](ledger::Result result) {
+          callback_called = true;
+        }, _1);
+
+  const std::string payment_id = "bob";
+  ON_CALL(*mock_ledger_impl_, GetPaymentId())
+      .WillByDefault(testing::ReturnRef(payment_id));
+
+  ledger::WalletInfoProperties wallet_info;
+  wallet_info.key_info_seed.push_back(1);
+  ON_CALL(*mock_ledger_impl_, GetWalletInfo())
+      .WillByDefault(testing::ReturnRef(wallet_info));
+
+  // Act
+  promotion_->ClaimTokens(promotion.Clone(), callback);
+
+  // Assert
+  EXPECT_FALSE(callback_called);
+}
 
 }  // namespace braveledger_promotion
 /*
